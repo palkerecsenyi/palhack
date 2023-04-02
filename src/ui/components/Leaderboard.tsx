6365d84e657e28b736472e559905d3af6a7750f4
Pal Kerecsenyi <pal@p.kerecs.com>
2023-04-02 01:14:29 +0100

    fix

--- src/ui/components/Leaderboard.tsx
+++ src/ui/components/Leaderboard.tsx
@@ -4,6 +4,8 @@ import { useLeaderboard } from "../data/leaderboard"
 export default function Leaderboard() {
     const leaderboard = useLeaderboard()
     return <div>
-        {leaderboard.map(entry => <div)}
+        {leaderboard.map(entry => <div key={entry.username}>
+            
+        </div>)}
     </div>
 }
