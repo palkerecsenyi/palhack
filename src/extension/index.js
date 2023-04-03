26b826a927640a304156da432abb54aa58554569
Hackintosh Five <git@hack5.dev>
2023-04-03 11:52:03 +0100

    Fix branding and stopship

--- src/extension/index.js
+++ src/extension/index.js
@@ -4,7 +4,6 @@ const currentUrl = window.location.origin
 switch (currentUrl) {
     case "https://www.amazon.co.uk":
     case "https://www.amazon.com":
-    case "null": // STOPSHIP
         await Amazon()
         break
 }
